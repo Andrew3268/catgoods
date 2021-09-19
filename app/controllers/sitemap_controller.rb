77a326312f281a5b97968f06f5c0af74ef5ad80c
Your <scarlet3628@likelion.org>
2021-09-19 15:05:33 +0900

    added errors

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,6 +1,6 @@
 class SitemapController < ApplicationController
   respond_to :xml
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/posts/hashtag/:name', to:'posts#hashtags'
+
+  get '/404' , to: 'errors#not_found'
+  get '/422' , to: 'errors#unacceptable'
+  get '/500' , to: 'errors#server_errors'
 end

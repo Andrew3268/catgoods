e0bd60c297c8652a1b9b60b03024d6d51bc2a1b5
Your <scarlet3628@likelion.org>
2022-04-25 15:48:58 +0900

    added chart

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,8 @@ import Rails from "@rails/ujs"
 // import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "chartkick/chart.js"
+
 
 Rails.start()
 Turbolinks.start()
